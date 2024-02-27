96800509d41f0b3551a27322fd457a2ff4149210
French II, Stephan Floyd <stffrenc@iu.edu>
2024-02-27 16:21:02 -0500

    Made the Controller Cross Origin

--- src/main/java/edu/iu/stffrenc/coffeeorder/controllers/HomeController.java
+++ src/main/java/edu/iu/stffrenc/coffeeorder/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package edu.iu.stffrenc.coffeeorder.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HomeController {
 
 
